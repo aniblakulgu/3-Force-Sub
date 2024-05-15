9242a20407166e96cf4095ba196f258decf7d6da
aniblakulgu <142817962+aniblakulgu@users.noreply.github.com>
2024-05-15 15:41:48 +0500

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -110,7 +110,7 @@ async def start_command(client: Client, message: Message):
 
 #=====================================================================================##
 
-WAIT_MSG = "<b>ᴡᴏʀᴋɪɴɢ....</b>"
+WAIT_MSG = "<b>Yuklanmoqda...</b>"
 
 REPLY_ERROR = "<code>Use this command as a reply to any telegram message without any spaces.</code>"
 
@@ -160,7 +160,7 @@ async def not_joined(client: Client, message: Message):
 async def get_users(client: Bot, message: Message):
     msg = await client.send_message(chat_id=message.chat.id, text=WAIT_MSG)
     users = await full_userbase()
-    await msg.edit(f"{len(users)} cur 2")
+    await msg.edit(f"{len(users)} <b>Foydalanuvchilar soni</b>")
 
 @Bot.on_message(filters.private & filters.command('broadcast') & filters.user(ADMINS))
 async def send_text(client: Bot, message: Message):
